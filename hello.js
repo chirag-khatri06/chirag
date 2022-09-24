ed4c1e017f920f05cf97e6b8ae62c8f73b355721
chirag061995 <114279568+chirag061995@users.noreply.github.com>
2022-09-24 17:00:24 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,20 @@
+
+
+var name="iacsd";
+var data=89;
+data=data+34;
+status=false;
+
+
+//#JSON object
+
+var contact={
+		"firstname":"chirag",
+		"lastname":"khatri",
+		"email":"chirag123@gmail.com",
+		"age":50,
+                              };    
+
+console.log("Hello from Application");
+console.log(data);
+consoe.log(contact);				
